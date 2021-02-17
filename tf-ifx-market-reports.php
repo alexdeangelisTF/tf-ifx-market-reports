b8353bf9b0bb164600a872c34ed9f6d7ce6e1770
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-17 12:07:10 +0000

    adding more description to plugin header

--- tf-ifx-market-reports.php
+++ tf-ifx-market-reports.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name:       TF IFX Payments Market Reports
- * Description:       WordPress plugin for IFX Payments that creates a post from a JSON feed
+ * Description:       WordPress plugin for IFX Payments that creates a post from a JSON feed via ACF Options fields
  * Version:           1.0.0
  * Author:            Thinking Fox
  * Author URI:        https://www.thinkingfox.com/
