4ce1853709c3df9dae72149b4791b10ecfbbdfb7
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-17 12:08:53 +0000

    only request the json if there is a value in the field

--- includes/get_json.php
+++ includes/get_json.php
@@ -3,18 +3,20 @@
 // Check if a JSON URL has been provided
 $json_url = get_field('ifx_market_report_json_url','option');
 
-$request = false;
-/*THE JSON CALL!*/
-$request = wp_remote_get( $json_url );
+if ($json_url) {
+	$request = false;
+	/*THE JSON CALL!*/
+	$request = wp_remote_get( $json_url );
 
-if( is_wp_error( $request ) ) {
-		return false; // Bail early
+	if( is_wp_error( $request ) ) {
+			return false; // Bail early
+	}
+
+	$body = wp_remote_retrieve_body( $request );
+	$data = json_decode( $body );
+	if( ! empty( $data ) ) {
+			$ifx_json_date = $data->date;
+			$ifx_json_text = $data->text;
+			$ifx_json_text = wpautop($ifx_json_text);
+	} else { }
 }
-
-$body = wp_remote_retrieve_body( $request );
-$data = json_decode( $body );
-if( ! empty( $data ) ) {
-		$ifx_json_date = $data->date;
-		$ifx_json_text = $data->text;
-		$ifx_json_text = wpautop($ifx_json_text);
-} else { }
