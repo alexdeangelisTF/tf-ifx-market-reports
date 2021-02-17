5f4169864335425e11733f3fa6e2e3fdb0e1fb48
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-17 10:57:06 +0000

    Header information for a WordPress plugin

---
+++ tf-ifx-market-reports.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Plugin Name:       TF IFX Payments Market Reports
+ * Description:       WordPress plugin for IFX Payments that creates a post from a JSON feed
+ * Version:           1.0.0
+ * Author:            Thinking Fox
+ * Author URI:        https://www.thinkingfox.com/
+ */
